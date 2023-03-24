0a71d1e30c4edea9a9b1055a88c6ec5db3b1d30e
Rajat Dua <rajatdua95@gmail.com>
2023-03-24 15:56:15 +0530

    refactor(index.js): separation of entites and error handling

--- index.js
+++ index.js
@@ -1,86 +1,87 @@
-const tmi = require('tmi.js');
-const mysql = require('mysql');
-const axios = require('axios');
-const fs = require('fs');
-const emoji = require('node-emoji');
+const tmi = require("tmi.js");
+const mysql = require("mysql");
+const fs = require("fs");
+const emoji = require("node-emoji");
+const utils = require("./utils");
+const { CHECK_LIVE_TIME, queries } = require("./constants");
 
 // Read config.json
-const config = JSON.parse(fs.readFileSync('config.json'));
+const config = utils.parseJSON(fs.readFileSync("config.json"), {});
+
+const { database = {}, twitch = {} } = config || {};
+
+const { host, user, password, name: databaseName } = database;
 
 const db = mysql.createConnection({
-  host: config.database.host,
-  user: config.database.user,
-  password: config.database.password,
-  database: config.database.name
+  host,
+  user,
+  password,
+  database: databaseName,
 });
 
 // Connect to the database
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to database: ', err);
+    console.error("Error connecting to database: ", err);
     return;
   }
-  console.log('Connected to database!');
+  console.log("Connected to database!");
 });
 
-// Twitch API call to check if channel is live
-async function checkIfLive(channel) {
-  const res = await axios.get(`https://api.twitch.tv/helix/streams?user_login=${channel}`, {
-    headers: {
-      'Client-ID': config.twitch.clientId,
-      'Authorization': `Bearer ${config.twitch.apioauth}`
-    }
-  });
-  return res.data.data.length > 0;
-}
+const { botUsername, botOauth, channel } = twitch;
 
 // Twitch configuration options
 const opts = {
   identity: {
-    username: config.twitch.botUsername,
-    password: config.twitch.botOauth
+    username: botUsername,
+    password: botOauth,
   },
-  channels: [
-    config.twitch.channel
-  ]
+  /* TODO: Is it possible to store the channels directly in an array under config.json */
+  channels: [channel],
 };
+
+// Global variable used in multiple functional scopes - TODO: Can this be scoped?
+let channelLive = false;
 
 // Create a client with the given options
 const client = new tmi.client(opts);
 
 // Register event handlers
-client.on('message', (channel, tags, message, self) => {
+client.on("message", (channel, tags, message, self) => {
   if (self) return; // Ignore messages from the bot
   const username = tags.username;
-  
+
   // Replace emojis with their text representation & remove invalid characters
-  const demojifiedMessage = emoji.unemojify(message).replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, '');
-  const sql = 'INSERT INTO chatlog (channel, username, message, live) VALUES (?, ?, ?, ?)';
-  console.log(`${channelLive}, ${channel} - ${username}: ${message}`)
-  db.query(sql, [channel, username, demojifiedMessage, channelLive], (err, result) => {
-    if (err) console.error('Error inserting into database: ', err);
-  });
+  const demojifiedMessage = emoji
+    .unemojify(message)
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, "");
+  const sql = queries.insert.chatLog;
+  console.log(`${channelLive}, ${channel} - ${username}: ${message}`);
+  db.query(
+    sql,
+    [channel, username, demojifiedMessage, channelLive],
+    (err, _) => {
+      if (err) console.error("Error inserting into database: ", err);
+    }
+  );
 });
 
-let channelLive = false;
-
 // Check channel status every minute
-setInterval(() => {
-  checkIfLive(opts.channels[0].slice(1))
-    .then((live) => {
-      channelLive = live;
-      console.log(`Channel ${opts.channels[0]} is ${live ? 'live' : 'offline'}`);
-    })
-    .catch((err) => {
-      console.error('Error checking if channel is live: ', err);
-    });
-}, 60000);
+setInterval(async () => {
+  /* TODO: Why can't we use the channel name directly from the configuration itself? Why is slice required? - slice(1) removes the first character from the channel name */
+  channelLive = await utils.checkIfLive(opts.channels[0].slice(1), config);
+  console.log(
+    `Channel ${opts.channels[0]} is ${channelLive ? "live" : "offline"}`
+  );
+  /* TODO: should this be part of configuration too? */
+}, CHECK_LIVE_TIME);
 
 // Connect to Twitch
-client.connect()
+client
+  .connect()
   .then(() => {
-    console.log('Connected to Twitch!');
+    console.log("Connected to Twitch!");
   })
   .catch((err) => {
-    console.error('Error connecting to Twitch: ', err);
+    console.error("Error connecting to Twitch: ", err);
   });
