e3e5e221d1d490401e1574cac554551db329546b
LinneB <77924330+LinneB@users.noreply.github.com>
2023-09-19 18:43:13 +0200

    Store client secret instead of token

--- utils.js
+++ utils.js
@@ -1,5 +1,23 @@
 const axios = require("axios");
 
+async function generateToken() {
+  const { clientid, secret } = logbot.config.twitch;
+  const response = await axios.post("https://id.twitch.tv/oauth2/token",
+    {
+      client_id: clientid,
+      client_secret: secret,
+      grant_type: "client_credentials"
+    },
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }
+  );
+  logbot.config.twitch.token = response.data.access_token;
+  updateLiveStatus();
+}
+
 async function updateLiveStatus() {
   const channels = logbot.config.twitch.channels;
   for (let i = 0; i < channels.length; i++) {
@@ -14,18 +32,21 @@ async function updateLiveStatus() {
           },
         },
       );
-      if (response.status === 200) {
-        logbot.livestatus[channel.toLowerCase()] = response.data.data.length > 0;
-        console.log(`INFO: ${channel.toLowerCase()} is ${response.data.data.length > 0 ? "online" : "offline"}`)
-      } else {
-        console.error(`ERROR: Twitch API request returned status code ${response.status}: ${response.data}`);
-      };
+      logbot.livestatus[channel.toLowerCase()] = response.data.data.length > 0;
+      console.log(`INFO: ${channel.toLowerCase()} is ${response.data.data.length > 0 ? "online" : "offline"}`)
     } catch(error) {
-      console.error("ERROR: Could not update live status: ", error.response.data);
+      if (logbot.config.twitch.token) {
+        console.info("INFO: Invalid token, generating a new one...");
+      } else {
+        console.info("INFO: Generating token...")
+      }
+      generateToken();
+      break;
     };
   };
 };
 
 module.exports = {
   updateLiveStatus,
+  generateToken,
 };
