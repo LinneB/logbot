a321d52e4a181d80e56fceb06ea4b3fe4029c655
LinneB <77924330+LinneB@users.noreply.github.com>
2024-02-19 12:34:01 +0100

    feat(config): Verify config parameters on startup

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,23 @@
 import fs from "fs";
 import toml from "toml";
 
+function validateConfig(config) {
+  const requiredOptions = {
+    "twitch": ["clientid", "secret", "channels"],
+    "database": ["host", "database", "user", "password"]
+  };
+  for (const section of Object.keys(requiredOptions)) {
+    if (!config[section]) {
+      throw new Error(`Missing ${section} section from config file`);
+    }
+    for (const option of requiredOptions[section]) {
+      if (!config[section][option]) {
+        throw new Error(`Missing ${option} option in ${section} section of config file`);
+      }
+    }
+  }
+}
+
 const config = toml.parse(fs.readFileSync("config.toml"));
+validateConfig(config);
 export default config;
